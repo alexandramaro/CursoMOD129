51814cfb00849584513e74017be5d0b722513757
alexandramaro <alexandramaro@hotmail.com>
2023-04-10 19:35:35 +0100

    Add project files.

---
+++ CursoMOD129/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using CursoMOD129.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CursoMOD129.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyView()
+        {
+            ViewData["AuthorName"] = "Alexandra Amaro";
+            return View();
+        }
+
+        //este é para o nome colocado no input do form
+        [HttpPost]
+        public IActionResult MyView(string author)
+        {
+            ViewData["AuthorName"] = author;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
